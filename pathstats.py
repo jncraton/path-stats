565cc71f3d8d3d23e6c8eb5c6109008901a717e1
Jon Craton <jncraton@gmail.com>
2022-08-25 10:27:41 -0400

    Reformatted with black

--- pathstats.py
+++ pathstats.py
@@ -1,15 +1,17 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Device:
     name: str
     bandwidth: float
     latency: float
 
+
 corporate = [
     Device("Router", 1000, 10.0),
-    Device("10G Ethernet Switch", 10000, 0.5), 
-    Device("Gigabit Ethernet Switch", 10000, 0.5), 
+    Device("10G Ethernet Switch", 10000, 0.5),
+    Device("Gigabit Ethernet Switch", 10000, 0.5),
     Device("802.11 AP", 450, 2.0),
 ]
 
@@ -18,13 +20,14 @@ home = [
     Device("Wireless Router", 450, 10.0),
 ]
 
+
 def get_max_bandwidth(path):
     """
     Returns the maximum bandwidth for a given `path`.
 
     A path can only deliver as much bandwidth as the lowest bandwidth link,
     so this effectively returns the Device with the lowest bandwidth
-    
+
     >>> get_max_bandwidth(corporate)
     450
 
@@ -36,13 +39,14 @@ def get_max_bandwidth(path):
 
     return 0
 
+
 def get_min_latency(path):
     """
     Returns the minimum latency for a given `path`.
 
     In order to calculate the path minimum latency, we must sum the
     latencies along the path.
-    
+
     >>> get_min_latency(corporate)
     13.0
 
@@ -54,14 +58,15 @@ def get_min_latency(path):
 
     return 0
 
+
 def get_bandwidth_bottleneck(path):
     """
     Returns the name of the device that limits the path bandwidth. This is
     the device on the path with the lowest bandwidth.
-    
+
     >>> get_bandwidth_bottleneck(corporate)
     '802.11 AP'
-    
+
     >>> get_bandwidth_bottleneck(home)
     'Cable Modem'
 
